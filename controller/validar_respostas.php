0f6bdd22728fc74443712a4bee97de3349fb8664
Rafael Evald <rafael-ev-silva@outlook.com>
2022-05-04 21:50:06 -0400

    Teste pegar dados da questão

--- controller/validar_respostas.php
+++ controller/validar_respostas.php
@@ -1,4 +1,17 @@
 <?php
     $user_name=$_POST['user_name'];
 
+    $questao_01=$_POST['questao_01'];
+    $questao_02=$_POST['questao_02'];
+    $questao_03=$_POST['questao_03'];
+    $questao_04=$_POST['questao_04'];
+    $questao_05=$_POST['questao_05'];
+    $questao_06=$_POST['questao_06'];
+    $questao_07=$_POST['questao_07'];
+    $questao_08=$_POST['questao_08'];
+    $questao_09=$_POST['questao_09'];
+    $questao_10=$_POST['questao_10'];
+
+    echo $questao_02;
+
 ?>
